88f170d18482846766336116f82af64b663c78aa
ThonyIucI <anthonyau011798@gmail.com>
2022-09-05 06:02:03 -0500

    dirname fixed

--- api/src/app.js
+++ api/src/app.js
@@ -13,14 +13,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../..', 'client', 'build')));
+// app.use(express.static(path.join(__dirname, '../..', 'client', 'build')));
 
 app.use(cors('*'));
 
 app.use('/api', routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../..', 'client', 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../..', 'client', 'build', 'index.html'));
+// });
 
 // app.disable('etag');
 export default app;
